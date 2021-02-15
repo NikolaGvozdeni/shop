dc5b15dcc6fccecaa1f6e2ffb7435cb0539a7064
Filip <andricfilip97@gmail.com>
2021-02-15 14:20:40 +0100

    initial commit

---
+++ src/main/java/com/cringeguy/CringeguyApplication.java
@@ -0,0 +1,13 @@
+package com.cringeguy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CringeguyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CringeguyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cringeguy/CringeguyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cringeguy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CringeguyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
